d9d81d2108b8af2893977686cd5acb0e24a85eb2
Robert Goddard <robert.goddard@morsco.com>
2023-08-10 14:38:34 -0500

    Add lombok and logging

---
+++ 002-lombok-logging-debugging/src/main/java/com/dialexa/lombokloggingdebugging/lombokloggingdebugging/models/Numbers.java
@@ -0,0 +1,29 @@
+package com.dialexa.lombokloggingdebugging.lombokloggingdebugging.models;
+
+public class Numbers {
+
+    private int a;
+    private int b;
+
+    public Numbers(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+}
