cc32a165fc60f591f3ea094d5ab85fde9f584986
Robert Goddard <robert.goddard@morsco.com>
2023-08-10 12:34:55 -0500

    Add boiler plate springboot project

---
+++ 001-my-first-endpoint/src/test/java/com/dialexa/myfirstendpoint/myfirstendpoint/MyFirstEndpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dialexa.myfirstendpoint.myfirstendpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstEndpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
