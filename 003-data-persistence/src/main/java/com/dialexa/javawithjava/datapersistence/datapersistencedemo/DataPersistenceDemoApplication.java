4a94f43d14106a3ab4a76c3897fc10b054e9f36d
Robert Goddard <robert.goddard@morsco.com>
2023-08-16 22:50:59 -0500

    Updating naming for main application class to follow java conventions

--- 003-data-persistence/src/main/java/com/dialexa/javawithjava/datapersistence/datapersistencedemo/DataPersistenceDemoApplication.java
+++ 003-data-persistence/src/main/java/com/dialexa/javawithjava/datapersistence/datapersistencedemo/DataPersistenceDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DatapersistencedemoApplication {
+public class DataPersistenceDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DatapersistencedemoApplication.class, args);
+		SpringApplication.run(DataPersistenceDemoApplication.class, args);
 	}
 
 }
